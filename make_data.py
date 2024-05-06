0caede73335c51d07e3e03f6544f37680de1a9da
duclong030202 <149302565+duclong030202@users.noreply.github.com>
2024-05-06 14:12:21 +0700

    Add files via upload

--- make_data.py
+++ make_data.py
@@ -2,38 +2,56 @@ import numpy as np
 import cv2
 import time
 import os
-
+from tkinter import *
 # Label: 00000 là ko cầm tiền, còn lại là các mệnh giá
-label = "200000"
+label = "600000"
 
 cap = cv2.VideoCapture(0)
 
 # Biến đếm, để chỉ lưu dữ liệu sau khoảng 60 frame, tránh lúc đầu chưa kịp cầm tiền lên
-i=0
-while(True):
+def makedata():
+    i=0
+    while(True):
     # Capture frame-by-frame
     #
-    i+=1
-    ret, frame = cap.read()
-    if not ret:
-        continue
-    frame = cv2.resize(frame, dsize=None,fx=0.3,fy=0.3)
+        i+=1
+        ret, frame = cap.read()
+        if not ret:
+            continue
+        frame = cv2.resize(frame, dsize=None,fx=0.3,fy=0.3)
 
     # Hiển thị
-    cv2.imshow('frame',frame)
+        cv2.imshow('frame',frame)
 
     # Lưu dữ liệu
-    if i>=60 and i<=1060:
-        print("Số ảnh capture = ",i-60)
+        if i>=60 and i<=1060:
+            print("Số ảnh capture = ",i-60)
         # Tạo thư mục nếu chưa có
-        if not os.path.exists('data/' + str(label)):
-            os.mkdir('data/' + str(label))
+            if not os.path.exists('data/' + str(label)):
+                os.mkdir('data/' + str(label))
 
-        cv2.imwrite('data/' + str(label) + "/" + str(i) + ".png",frame)
+            cv2.imwrite('data/' + str(label) + "/" + str(i) + ".png",frame)
 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        if cv2.waitKey(1) & 0xFF == ord('q'):
+            break
+    cap.release()
+    cv2.destroyAllWindows()
+    return
 
+window = Tk()
+window.geometry("640x640")
+button=Button(window,text="Tao du lieu!!",command=makedata,font=("Comic Sans",30))
+
+button.pack()
+button.place(x=200,y=220)
+button2=Button(window,text="Dong cua so",command=window.destroy,font=("Comic Sans",30))
+button2.pack()
+button2.place(x=200,y=300)
+photo = PhotoImage(file='camera.png')
+label1 = Label(window,text="Nhan dang tien VN",font=('Arial',40,'bold'),
+            fg = 'green', relief=RAISED, bd=5, padx=10, pady=10, image = photo, compound = 'bottom')
+label1.place(x=100,y=10)
+label2=Label(window,text="An Q de dung camera",font=('Arial',40,'underline'))
+label2.place(x=100,y=400)
+window.mainloop()
 # When everything done, release the capture
-cap.release()
-cv2.destroyAllWindows()
